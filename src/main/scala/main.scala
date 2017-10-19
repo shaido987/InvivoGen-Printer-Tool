5c2402a8db6be67272a7099a440f4f21f9342311
shaido987 <shaido987@hotmail.com>
2017-10-19 23:47:56 +0800

    Added checks and prints for Orders, made some adjustments on how things are printed.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -13,29 +13,41 @@ object invivogenTDSPrinter {
     val baseAdress  = "http://invivogen.com/"
     val destFolder  = "downloaded TDSs/"
     
-    val orders  = getOrders(orderFile)
-    val linkMap = getLinkMap(linkFile)
+    val orders:  Map[String, Int]    = getOrders(orderFile)
+    val linkMap: Map[String, String] = getLinkMap(linkFile)
 
-    //downloadAllTDS(linkMap, baseAdress, destFolder)
-    downloadOrderTDS(linkMap, orders.map(_._1).toSeq, baseAdress, destFolder)
-    printer.printOrders(destFolder, orders)
+    // Filter orders after names in linkMap
+    // Needed as not all orders are to-be-printed TDSs
+    val tdsOrders   = orders filterKeys linkMap.keys.toSet
+    val otherOrders = orders filterKeys tdsOrders.keys.toSet
+
+    // Printing information about the orders
+    println("-----------------------")
+    println(s"Orders in TDS link list, total: ${tdsOrders.size}")
+    tdsOrders.keys foreach println
+    println(s"\nOrders not in TDS list, total: ${otherOrders.size}")
+    otherOrders.keys foreach println
+    println("-----------------------")
+
+    downloadOrderTDS(linkMap, tdsOrders.keys.toSeq, baseAdress, destFolder)
+    printer.printOrders(destFolder, tdsOrders)
   }
 
   def getOrders(orderFile: String): Map[String, Int] = {
     val orders = Source.fromFile(orderFile).getLines().drop(1)
 
     val res = for (order <- orders if order.trim.nonEmpty) yield {
-      val name :: num :: _ = order.split(",").map(_.trim()).toList
-      (name -> num.toInt)
+      val name :: numCopies :: _ = order.split(",").map(_.trim.replace("\"", "")).toList
+      (name -> numCopies.toInt)
     }
-    res.toMap
+    res.filter(_._2 > 0).toMap  // Only return the orders with 1 or more to-be-printed pdf
   }
   
   def getLinkMap(linkFile: String): Map[String, String] = {
     val nameLinks = Source.fromFile(linkFile).getLines().drop(1)
 
     val res = for (nameLink <- nameLinks if nameLink.trim.nonEmpty) yield {
-      val name :: link :: _ = nameLink.split(",").map(_.trim()).toList
+      val name :: link :: _ = nameLink.split(",").map(_.trim).toList
       (name -> link)
     }
     res.toMap
@@ -49,12 +61,12 @@ object invivogenTDSPrinter {
 
   def downloadOrderTDS(linkMap: Map[String, String], names: Seq[String], baseAdress: String, destFolder: String): Unit = {
     for ((name, index) <- names.zipWithIndex) {
-      val link = linkMap.get(name) match {
-        case Some(link) => link
-        case None       => { throw new NoSuchElementException("The name " + name + " not in link csv file."); ""}
+      println(s"${index+1}/${names.length}\t$name")
+
+      linkMap.get(name) match {
+        case Some(link) => downloadTDS(name, link, baseAdress, destFolder)
+        case None       => throw new NoSuchElementException("The name " + name + " not in link csv file.")
       }
-      println(s"${index+1}/${names.length}\t$name\t$link")
-      downloadTDS(name, link, baseAdress, destFolder)
     }
   }
 
--- src/main/scala/printer.scala
+++ src/main/scala/printer.scala
@@ -3,29 +3,43 @@ package logic
 import java.io.File
 import java.awt.print.PrinterJob
 import java.awt.print.PrinterException
+
 import javax.print.PrintService
+import javax.print.attribute.HashPrintRequestAttributeSet
+import javax.print.attribute.PrintRequestAttributeSet
+import javax.print.attribute.standard.PageRanges
+import javax.print.attribute.standard.Sides
+import javax.print.attribute.standard.Copies
+
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.printing.PDFPageable
 
+
 object printer {
   
-  lazy val printJob = PrinterJob.getPrinterJob()
-  lazy val printer  = if (printJob.printDialog()) Some(printJob.getPrintService()) else None
+  //lazy val printJob = PrinterJob.getPrinterJob()
+  //lazy val printer  = if (printJob.printDialog()) Some(printJob.getPrintService()) else None
 
-  def print(pdf: PDDocument, ps: PrintService): Unit = {
+  private def print(pdf: PDDocument, numCopies: Int): Unit = {
     val job = PrinterJob.getPrinterJob()
-    job.setPrintService(ps)
+    //job.setPrintService(ps)
     job.setPageable(new PDFPageable(pdf))
-    job.print()
+
+    val attr = new HashPrintRequestAttributeSet()
+    attr.add(new PageRanges(1, 2)) // pages 1 and 2
+    attr.add(Sides.TWO_SIDED_LONG_EDGE)
+    attr.add(new Copies(numCopies))
+    job.print(attr) //silent print ?
   }
 
-  def printPDF(file: File, printer: Option[PrintService] = printer): Unit = {
+  def printPDF(file: File, numCopies: Int): Unit = { //, printer: Option[PrintService] = printer): Unit = {
     if (file.getName().endsWith("pdf")) {
       val pdf = PDDocument.load(file)
-      printer match {
-        case Some(ps) => print(pdf, ps)
-        case None     => throw new PrinterException("No printer found")
-      }
+      print(pdf, numCopies)
+//      printer match {
+//        case Some(ps) => print(pdf, ps)
+//        case None     => throw new PrinterException("No printer found")
+//      }
     } else {
       throw new PrinterException(file.getName() + " is not a pdf.")
     }
@@ -34,15 +48,17 @@ object printer {
   def printDirectory(dir: String): Unit = {
     val d = new File(dir) 
     for (file <- d.listFiles if file.getName().endsWith(".pdf")) {
-      printPDF(file)
+      printPDF(file, 1)
     }
   }
 
   def printOrders(dir: String, orders: Map[String, Int]): Unit = {
-    for ((name, num) <- orders) {
-      val file = new File(dir + name + ".pdf")
-      printPDF(file)
+    //for ((name, num) <- orders) {
+    val (name, numCopies) = orders.head
+    val file = new File(dir + name + ".pdf")
+    //printPDF(file, numCopies)
+    printPDF(file, 1)
       // TODO: prinitng here, num copies
-    }
+   // }
   }
 }
